5a52148bc7bafb601d68e090c97959033e04d11b
Sági János <sagi.janos77@gmail.com>
2018-04-10 21:10:23 +0200

    i changed the ckeditor

--- public/javascripts/ckeditor.js
+++ public/javascripts/ckeditor.js
@@ -1,1 +1,8 @@
-CKEDITOR.replace('description');
+ClassicEditor
+    .create(document.querySelector('#editor'), {
+        removePlugins: ['Heading', ],
+        toolbar: ['bold', 'italic', 'bulletedList', 'numberedList', 'blockQuote', 'link', 'Image', 'underline', 'strikethrough', 'code']
+    })
+    .catch(error => {
+        console.log(error);
+    });
